c4ff7bced9f95f2340a97f616b6bdcddc4b904b0
Justice Smith <jcode94ttv@gmail.com>
2022-03-21 23:27:34 -0400

    expo,react native and paper

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
