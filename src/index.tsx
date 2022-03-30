d331cfa35d23099de312792d49aba85b016b76e8
clarkee <69399383+ClarkeeCity@users.noreply.github.com>
2022-03-30 10:13:07 -0400

    styling and added items for drawer nav

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+import { 
+  Drawer as PaperDrawer, 
+  Divider,
+  Button,
+} from 'react-native-paper';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import Splash from './screen/Splash';
 import Login from './screen/Login';
 import Home from'./screen/Home';
+import { theme } from './themes/Theme';
 
 const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
@@ -17,10 +29,45 @@ const tokenVal = true;
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+    drawerContent={() => 
+      <>
+        <View>
+          <Text>We can put a greeting and some stuff up here. :):)</Text>
+          <Divider />
+        </View>
+        <View>
+          <PaperDrawer.Section title="My Account">
+            <Button icon="account" mode="contained" style={styles.drawerButton} onPress={() => console.log('profile screen')}>Profile</Button>
+          </PaperDrawer.Section>
+          <PaperDrawer.Section title="Other">
+            <Button icon="cog" mode="contained" style={styles.drawerButton} onPress={() => console.log('settings screen')}>Settings</Button>
+            <View style={{height: 5}}></View>
+            <Button icon="help-circle-outline" mode="contained" style={styles.drawerButton} onPress={() => console.log('help screen')}>Help</Button>
+          </PaperDrawer.Section>
+        </View>
+        <Button mode="outlined" style={{marginHorizontal: 10, marginTop: 15}} onPress={() => console.log('log the user out.')}>Log Out</Button>
+      </>}
+    >
       {tokenVal == false ? 
-      ( <Drawer.Screen name="Login" component={Login} options={{headerShown: false}}/> ) :
-      ( <Drawer.Screen name="Home" component={Home} /> )}
+        ( <Drawer.Screen 
+            name="Login" 
+            component={Login} 
+            options={{headerShown: false}} 
+          /> ) :
+        ( <Drawer.Screen 
+          name="Clovebook" 
+          component={Home} 
+          options={{
+            headerTintColor: theme.colors.text_light, 
+              headerStyle: {
+              backgroundColor: theme.colors.selected,
+            }
+          }}
+        /> )
+      }
+      <Drawer.Screen name="Settings" children={() => <Text>Settings screen.</Text>} />
+      <Drawer.Screen name="Help" children={() => <Text>Help screen.</Text>} />
     </Drawer.Navigator>
   )
 }
@@ -40,5 +87,12 @@ const App = () => {
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  drawerButton: {
+    marginHorizontal: 10,
+    paddingVertical: 2,
+  }
+});
 
 export default App;
